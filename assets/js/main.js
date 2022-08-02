0c38233e6e58161e525edb71b6f0624d28a3dd30
DevMohamedElshazly <moshazly416@gmail.com>
2022-08-03 01:10:00 +0200

    add Header & Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,38 @@
-/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/*=============== SHOW MENU ===============*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navMenu.classList.remove("show-menu");
+}
+
+navLinks.forEach((link) => link.addEventListener("click", linkAction));
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
-
 /*=============== CALCULATE JS ===============*/
-
 
 /*=============== EMAIL JS ===============*/
